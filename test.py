406f13de502a7010688a536d920ed808dcf5eb68
kanhaiyasinha <sinhakanha21@gmail.com>
2024-03-07 14:09:55 +0530

    Updated UI - with system info button feature

---
+++ test.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('finalDashBoard.html')
+
+@app.route('/parameter')
+def parameter():
+    # Read the Excel file into a Pandas DataFrame
+    path = os.getcwd()
+    excel_file = os.path.join(path, 'system_overview.xlsx')  # Replace 'your_excel_file.xlsx' with your actual file path
+    df = pd.read_excel(excel_file)
+
+    # Render the HTML template with the DataFrame
+    return render_template('data99.html', data=df.to_html(index=False))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
